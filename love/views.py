bdc7d4f6b66a265298f01ff3fee3e3c54c753dc3
Taranjeet <reachtotj@gmail.com>
2015-02-13 23:45:48 +0530

    matching working

--- love/views.py
+++ love/views.py
@@ -8,6 +8,7 @@ from love.models import *
 def index(request):
 	c = {}
 	c.update(csrf(request))
+	existCrush=None
 	if request.POST:
 		print 'post request'
 		name=request.POST['name']
@@ -16,4 +17,8 @@ def index(request):
 		cname=request.POST['cname']
 		cbranch=request.POST['cbranch']
 		print name,branch,email,cname,cbranch
-	return render_to_response('index.html',context_instance=RequestContext(request))
+		newCrush=Crush(name=name,branch=branch,email=email,cname=cname,cbranch=cbranch)
+		newCrush.save()
+		#find the crush 
+		existCrush =  Crush.objects.filter(name=cname,branch=cbranch,cname=name,cbranch=branch)
+	return render_to_response('index.html',{'crush':existCrush},context_instance=RequestContext(request))
